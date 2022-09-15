41b65134625e3d5642b5ea4b29d1e8ab13b7cc63
AlexeyBolt <boltava.av@gmail.com>
2022-09-16 01:03:12 +0300

    Добавил решение задачи 3

---
+++ hw13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+if (number % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
