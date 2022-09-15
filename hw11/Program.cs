fb5e1e198db007dcf7cb1cd046ba02ba3b177bbc
AlexeyBolt <boltava.av@gmail.com>
2022-09-16 00:42:32 +0300

    Добавил решение Домашней задачи 1

---
+++ hw11/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+string input = Console.ReadLine();
+int a = int.Parse(input);
+Console.WriteLine("Введите число Б");
+string input2 = Console.ReadLine();
+int b = int.Parse(input2);
+int max = 0;
+int min = 0;
+
+if (a > b) max = a;
+if (a > b) min = b;
+if (b > a) max = b;
+if (b > a) min = a;
+
+Console.Write("Большее число =");
+Console.WriteLine(max);
+Console.Write("Меньшее число =");
+Console.WriteLine(min);
+
