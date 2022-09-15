787a5ce16e7bfad3dd222d957016e26ebae2ad33
AlexeyBolt <boltava.av@gmail.com>
2022-09-16 00:56:00 +0300

    Добавил решение задачи 2

---
+++ hw12/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+string input = Console.ReadLine();
+int a = int.Parse(input);
+Console.WriteLine("Введите число B");
+string input2 = Console.ReadLine();
+int b = int.Parse(input2);
+Console.WriteLine("Введите число C");
+string input3 = Console.ReadLine();
+int c = int.Parse(input3);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("Большее число =");
+Console.WriteLine(max);
