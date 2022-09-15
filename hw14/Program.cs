585b334109645229d5ec3174b048e3baef9ebce4
AlexeyBolt <boltava.av@gmail.com>
2022-09-16 02:09:53 +0300

    Добавил решение задачи 4

---
+++ hw14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите положительное число");
+int num = int.Parse(Console.ReadLine());
+
+int count = 2;
+
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
